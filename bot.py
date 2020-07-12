b8c4885cfa1129274d10761ab2d5d6e95b678175
Quantumato <kye2@vols.utk.edu>
2020-07-12 18:18:32 -0400

    Member join message (not working)

--- bot.py
+++ bot.py
@@ -11,14 +11,13 @@ client = discord.Client()
 
 @client.event
 async def on_ready():
-    #guild = discord.utils.find(lambda g: g.name == GUILD, client.guilds)
-    guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} has connected to Discord!\n'
-        f'{guild.name}(id: {guild.id})'
-    )
+    print(f'{client.user.name} has connected to Discord!\n')
 
-    members = '\n - '.join([member.name for member in guild.members])
-    print(f'Guild Members:\n - {members}')
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel_send(
+        f'Hi {member.name}, welcome to our little shitfest'
+        )
 
 client.run(TOKEN)
